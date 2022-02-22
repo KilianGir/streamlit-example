70c5febacafd281f4947f2018cf56f01169fd49c
KilianGir <97527152+KilianGir@users.noreply.github.com>
2022-02-22 22:23:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,9 @@ import plotly.graph_objects as go
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# First motorsport data app
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+All qualifications laps for top3 drivers in Formula 1 2021 Belgian GP:
 """
 df = pd.read_csv('./Top3QBelgiumGP2021.csv')
 df.head()
